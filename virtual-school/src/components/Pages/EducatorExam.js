2d3bc6a297b8bda2a0b4cb25f02fb979e3b8e7d8
Rawllings Kiprop <rutojr01@gmail.com>
2023-05-01 10:40:06 +0300

    Errors

--- virtual-school/src/components/Pages/EducatorExam.js
+++ virtual-school/src/components/Pages/EducatorExam.js
@@ -1,24 +1,14 @@
-import React, { useEffect, useState } from "react";
-import CoursesExam from "./CoursesExam";
+import React, { useEffect, useState } from 'react'
+import CoursesExam from './CoursesExam';
 import EducatorSideBar from "./EducatorSideBar";
 
 function EducatorExam() {
-  const token = localStorage.getItem("jwt");
-  const [educatorId, setEducatorId] = useState();
-  const [courses, setCourses] = useState([]);
-
-  useEffect(() => {
-    fetch("/loggedin", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setEducatorId(data.educator.id);
-        fetch(`/educator_courses/${data.educator.id}`, {
+    const token = localStorage.getItem("jwt");
+    const [educatorId, setEducatorId] = useState();
+    const [courses, setCourses] = useState([]);
+    
+    useEffect(() => {
+        fetch("/loggedin", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
@@ -26,32 +16,38 @@ function EducatorExam() {
           },
         })
           .then((res) => res.json())
-          .then((data) => setCourses(data));
-      });
-  }, []);
+          .then((data) => {
+          setEducatorId(data.educator.id);
+            fetch(`/educator_courses/${data.educator.id}`, {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            })
+              .then((res) => res.json())
+              .then((data) => setCourses(data));
+          });
+      }, []);
   return (
     <>
-      <div
-        className="text-3xl"
-        style={{
-          text: "center",
-          paddingTop: "120px",
-          paddingLeft: "0px",
-        }}
-      >
-        <div className="flex flex-wrap justify-center">
-          {courses.map((course) => (
-            <CoursesExam
-              key={course.course_name}
-              courseId={course.id}
-              name={course.course_name}
-            />
-          ))}
-        </div>
+    <div
+      className="text-3xl"
+      style={{
+        text: "center",
+        paddingTop: "120px",
+        paddingLeft: "0px",
+      }}
+    >
+      <div className="flex flex-wrap justify-center">
+        {courses.map((course) => (
+          <CoursesExam key={course.course_name} courseId={course.id} name={course.course_name} />
+        ))}
       </div>
-      <EducatorSideBar />
-    </>
-  );
+    </div>
+    <EducatorSideBar />
+  </>
+  )
 }
 
-export default EducatorExam;
+export default EducatorExam
--- virtual-school/src/components/Pages/EducatorSideBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function EducatorSideBar() {
-  return (
-    <div>EducatorSideBar</div>
-  )
-}
-
-export default EducatorSideBar
